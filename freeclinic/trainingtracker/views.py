93c1fcd4c93397158453c436928dcf95b614dfb3
quincy <quincyhuynh38@gmail.com>
2014-11-25 19:34:25 -0800

    Settings Template and Form

--- freeclinic/trainingtracker/views.py
+++ freeclinic/trainingtracker/views.py
@@ -134,8 +134,8 @@ def logout_view(request):
 class SettingsForm(forms.Form):
     username = forms.CharField(label='Username: ', max_length = 100)
     email = forms.CharField(label='Email: ', max_length = 100)
-    fname = forms.CharField(label='First name: ', max_length = 50)
-    lname = forms.CharField(label='Last name: ', max_length = 50)
+    firstname = forms.CharField(label='First name: ', max_length = 50)
+    lastname = forms.CharField(label='Last name: ', max_length = 50)
 
 
 class Settings(View):
