859ecc528939e2b1efa6b78135118eb9b81db737
Zachary Schneider <zschneider@berkeley.edu>
2014-12-01 19:22:19 -0800

    fixed settings

--- freeclinic/settings.py
+++ freeclinic/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'freeclinic.trainingtracker',
+    'trainingtracker',
 )
 
 MIDDLEWARE_CLASSES = (
